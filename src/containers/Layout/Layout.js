f27362978acc99109a1b39b1ab39016ef0a116a6
Shaowei <zsw22@vip.qq.com>
2017-11-30 00:06:59 +1100

    create folder for Aux component

--- src/containers/Layout/Layout.js
+++ src/containers/Layout/Layout.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import Aux from '../../hoc/Aux';
+import Aux from '../../hoc/Aux/Aux';
 import classes from './Layout.css';
 import Toolbar from '../../components/Navigation/Toolbar/Toolbar';
 import SideDrawer from '../../components/Navigation/SideDrawer/SideDrawer';
