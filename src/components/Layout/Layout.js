0479929352bf88086a41985ce46a56e8f6a4bf67
Shaowei <zsw22@vip.qq.com>
2017-11-29 23:21:43 +1100

    resuing backdrop

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,17 +1,30 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Aux from '../../hoc/Aux';
 import classes from './Layout.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-const layout = (props) => (
-    <Aux>
-        <Toolbar/>
-        <SideDrawer/>
-        <main className={classes.Content}>
-            {props.children}
-        </main>
-    </Aux>
-);
+class layout extends Component {
+    state = {
+        showSideDrawer: true
+    };
+    sideDrawerCloseHandler = () => {
+        this.setState({showSideDrawer: false});
+    };
+
+    render() {
+        return (
+            <Aux>
+                <Toolbar/>
+                <SideDrawer
+                    open={this.state.showSideDrawer}
+                    closed={this.sideDrawerCloseHandler}/>
+                <main className={classes.Content}>
+                    {this.props.children}
+                </main>
+            </Aux>
+        )
+    }
+}
 
 export default layout;
