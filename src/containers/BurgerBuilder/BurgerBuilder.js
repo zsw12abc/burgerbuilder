8cf14c46d98f33a97882a6dcdc97b5e7fc477c91
Shaowei <zsw22@vip.qq.com>
2017-11-30 13:56:23 +1100

    handling error by adding a errorhandler globally

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -7,6 +7,7 @@ import Modal from "../../components/UI/Modal/Modal";
 import OrderSummary from "../../components/Burger/OrderSummary/OrderSummary";
 import axios from '../../axios-orders';
 import Spinner from '../../components/UI/Spinner/Spinner';
+import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
 
 const INGREDIENT_PRICES = {
     salad: 0.5,
@@ -92,7 +93,7 @@ class BurgerBuilder extends Component {
             },
             deliveryMethod: 'fastest'
         };
-        axios.post('/orders.json', order)
+        axios.post('/orders.json', order)//send order summary as json to my server
             .then(response => {
                 console.log(response);
                 this.setState({loading: false, purchasing: false});
@@ -137,4 +138,4 @@ class BurgerBuilder extends Component {
 }
 
 
-export default BurgerBuilder;
+export default withErrorHandler(BurgerBuilder, axios);
