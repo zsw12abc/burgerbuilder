8e247f151a1eb7517c75ec4a550b4a0bd9f3e0af
Shaowei <zsw22@vip.qq.com>
2017-12-02 21:50:30 +1100

    remove checkout button on the nav

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -6,7 +6,7 @@ const navigationItems = (props) => (
     <ul className={classes.NavigationItems}>
         <NavigationItem link='/' exact>Burger Builder</NavigationItem>
         <NavigationItem link='/orders' >Orders</NavigationItem>
-        <NavigationItem link='/checkout' >Checkout</NavigationItem>
+        {/*<NavigationItem link='/checkout' >Checkout</NavigationItem>*/}
     </ul>
 );
 
