c282f9ac8e960ff3ba790de633c45493e054ecba
Shaowei <zsw22@vip.qq.com>
2017-12-02 21:20:41 +1100

    output ingredient in order

--- src/containers/Checkout/ContractData/ContractData.js
+++ src/containers/Checkout/ContractData/ContractData.js
@@ -22,7 +22,7 @@ class ContractData extends Component {
         this.setState({loading: true});
         const order = {
             ingredients: this.props.ingredients,
-            price: this.props.totalPrice,
+            price: this.props.price,
             customer: {
                 name: 'Shaowei',
                 address: {
