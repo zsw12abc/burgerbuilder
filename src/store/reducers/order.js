de5948e92631ed0b423be80843a29d6d4df3e303
Shaowei <zsw22@vip.qq.com>
2017-12-05 21:27:58 +1100

    order reducer

---
+++ src/store/reducers/order.js
@@ -0,0 +1,30 @@
+import * as actionTyptes from '../actions/actionTypes';
+
+const initialState = {
+    orders: [],
+    loading: false
+};
+
+const reducer = (state = initialState, action) => {
+    switch (Action.type) {
+        case actionTyptes.PURCHASE_BURGER_SUCCESS:
+            const newOrder = {
+                ...action.orderData,
+                id: action.orderId
+            };
+            return {
+                ...state,
+                loading: false,
+                orders: state.orders.concat(newOrder)
+            };
+        case actionTyptes.PURCHASE_BURGER_FAIL:
+            return {
+                ...state,
+                loading: false,
+            };
+        default:
+            return state;
+    }
+};
+
+export default reducer;
