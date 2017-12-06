07b052d4b023f0f29a41940c118c773f916873a0
Shaowei <zsw22@vip.qq.com>
2017-12-06 16:31:10 +1100

    store the token

--- src/index.js
+++ src/index.js
@@ -10,24 +10,26 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import burgerBuilderReducer from './store/reducers/burgerBuilder';
 import orderReducer from './store/reducers/order';
+import authReducer from './store/reducers/auth';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const rootReducer = combineReducers({
-    burgerBuilder: burgerBuilderReducer,
-    order: orderReducer
+	burgerBuilder: burgerBuilderReducer,
+	order: orderReducer,
+	auth: authReducer
 });
 
 const store = createStore(rootReducer, composeEnhancers(
-    applyMiddleware(thunk)
+	applyMiddleware(thunk)
 ));
 
 const app = (
-    <Provider store={store}>
-        <BrowserRouter>
-            <App/>
-        </BrowserRouter>
-    </Provider>
+	<Provider store={store}>
+		<BrowserRouter>
+			<App/>
+		</BrowserRouter>
+	</Provider>
 );
 
 ReactDOM.render(app, document.getElementById('root'));
