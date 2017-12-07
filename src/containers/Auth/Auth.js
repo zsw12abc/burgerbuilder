43a06d7a07863cf0c0fbb1e0be442f99aaf0163e
Shaowei <zsw22@vip.qq.com>
2017-12-07 11:27:38 +1100

    using updateobject in the entire app

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -8,6 +8,7 @@ import Button from '../../components/UI/Button/Button';
 import classes from './Auth.css';
 import * as actions from '../../store/actions/index';
 import Spinner from '../../components/UI/Spinner/Spinner';
+import {updateObject} from "../../shared/utility";
 
 class Auth extends Component {
 	state = {
@@ -71,15 +72,13 @@ class Auth extends Component {
 	}
 
 	inputChangedHandler = (event, controlName) => {
-		const updatedControls = {
-			...this.state.controls,
-			[controlName]: {
-				...this.state.controls[controlName],
+		const updatedControls = updateObject(this.state.controls, {
+			[controlName]: updateObject(this.state.controls[controlName], {
 				value: event.target.value,
 				valid: this.checkValidity(event.target.value, this.state.controls[controlName].validation),
 				touched: true
-			}
-		};
+			})
+		});
 		this.setState({controls: updatedControls});
 	};
 	submitHandler = (event) => {
