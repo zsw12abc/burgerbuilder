67cd7a9b1927d99499a3855ca50f9fce5655f9e3
Shaowei <zsw22@vip.qq.com>
2017-12-07 21:53:18 +1100

    unit testing

--- src/components/Navigation/NavigationItems/NavigationItems.test.js
+++ src/components/Navigation/NavigationItems/NavigationItems.test.js
@@ -20,4 +20,9 @@ describe('<NavigationItems/>', () => {
 		wrapper.setProps({isAuthenticated: true});
 		expect(wrapper.find(NavigationItem)).toHaveLength(3);
 	});
+
+	it("should render one <NavigationItem /> elements if authenticated", function () {
+		wrapper.setProps({isAuthenticated: true});
+		expect(wrapper.contains(<NavigationItem link={'/logout'}>Logout</NavigationItem>)).toEqual(true);
+	});
 });
